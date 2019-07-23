6b50c355c8b7999c541a783df222222be74c5d88
Fabio Santos <supergrilo@gmail.com>
2019-07-23 22:17:59 +0100

    zabbix sender untested

--- zb-tracer.py
+++ zb-tracer.py
@@ -1,15 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import minimalmodbus
 import datetime
+import socket
+import sys
+
+from pyzabbix import ZabbixMetric, ZabbixSender
+
+hostname = socket.gethostname()
+packet =[]
 
 def readNumberAndSaveToTable(instrument, address, table):
+	global packet
 	try:
-   		reading = instrument.read_register(address, 2, 4)
-   		print("Reading " + table + ": " + str(reading))
+                reading = instrument.read_register(address, 2, 4)
+                packet.append([ZabbixMetric(hostname, table, str(reading))])
 	except IOError:
 		print("Failed to read from instrument")
 
-instrument = minimalmodbus.Instrument('/dev/ttyXRUSB0', 1)
+instrument = minimalmodbus.Instrument(sys.argv[1], 1)
 instrument.serial.baudrate = 115200
 instrument.serial.bytesize = 8
 instrument.serial.parity   = minimalmodbus.serial.PARITY_NONE
@@ -19,26 +27,19 @@ instrument.mode = minimalmodbus.MODE_RTU
 
 #Read Solar Voltage
 readNumberAndSaveToTable(instrument, 0x3100, "solar_voltage")
-
 #Read Battery Voltage
 readNumberAndSaveToTable(instrument, 0x3104, "battery_voltage")
-
 #Read Solar Amps
 readNumberAndSaveToTable(instrument, 0x3101, "solar_amps")
-
 #Read Battery Amps
 readNumberAndSaveToTable(instrument, 0x3105, "battery_amps")
-
 #Read Battery Amps
 readNumberAndSaveToTable(instrument, 0x310D, "load_amps")
-
 #Read Battery Temperature
 readNumberAndSaveToTable(instrument, 0x311B, "temperature")
-
 #Read Daily Use
 readNumberAndSaveToTable(instrument, 0x3304, "daily_kw_used")
-
 #Read Daily Generated
 readNumberAndSaveToTable(instrument, 0x330C, "daily_kw_generated")
-
-print("FINISHED")
+print(packet)
+result = ZabbixSender(use_config=True).send(packet)
