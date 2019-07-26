c520bb9d5b99fea88546a050963836fa351bb12d
Fabio Santos <supergrilo@gmail.com>
2019-07-26 21:09:24 +0100

    monitoring new items

--- zb-tracer.py
+++ zb-tracer.py
@@ -25,21 +25,26 @@ instrument.serial.stopbits = 1
 instrument.serial.timeout  = 1.2
 instrument.mode = minimalmodbus.MODE_RTU
 
-#Read Solar Voltage
-readNumberAndSaveToTable(instrument, 0x3100, "solar_voltage")
-#Read Battery Voltage
+#Battery 
+
 readNumberAndSaveToTable(instrument, 0x3104, "battery_voltage")
-#Read Solar Amps
-readNumberAndSaveToTable(instrument, 0x3101, "solar_amps")
-#Read Battery Amps
 readNumberAndSaveToTable(instrument, 0x3105, "battery_amps")
-#Read Battery Amps
-readNumberAndSaveToTable(instrument, 0x310D, "load_amps")
-#Read Battery Temperature
 readNumberAndSaveToTable(instrument, 0x311B, "temperature")
-#Read Daily Use
+readNumberAndSaveToTable(instrument, 0x3200, "batt_status")
+readNumberAndSaveToTable(instrument, 0x311A, "batt_remain")
+
+#Solar
+
+readNumberAndSaveToTable(instrument, 0x3100, "solar_voltage")
+readNumberAndSaveToTable(instrument, 0x3101, "solar_amps")
+
+#Controller 
+
+readNumberAndSaveToTable(instrument, 0x3111, "temp_inside")
+readNumberAndSaveToTable(instrument, 0x3112, "temp_heatsink")
+readNumberAndSaveToTable(instrument, 0x310D, "load_amps")
 readNumberAndSaveToTable(instrument, 0x3304, "daily_kw_used")
-#Read Daily Generated
 readNumberAndSaveToTable(instrument, 0x330C, "daily_kw_generated")
+
 print(packet)
-result = ZabbixSender(use_config=True).send(packet)
+#result = ZabbixSender(use_config=True).send(packet)
